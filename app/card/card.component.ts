56032b2c7114969ce502031252b85dde3e084369
Fauziku2 <M.fauzi_87@hotmail.com>
2017-02-11 13:27:23 +0800

    added card component

---
+++ app/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  moduleId: module.id,
+  selector: "app-card",
+  templateUrl: "card.component.html",
+  styleUrls: [ "card.component.css" ] // array because we might have more than one style css file
+})
+
+export class CardComponent {}
