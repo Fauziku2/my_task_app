90243b1a8c5822405b5965081fbaa1faaa9ba853
Fauziku2 <M.fauzi_87@hotmail.com>
2017-02-11 20:26:08 +0800

    form added and form styling done

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,8 @@ import { Task } from "./model/task";
 @Component({
   moduleId: module.id, // so that we can use templateUrl
   selector: "my-app",
-  templateUrl: "app.component.html"
+  templateUrl: "app.component.html",
+  styleUrls: [ "app.component.css" ]
 })
 
 // private - nothing outside of this component can modified this data
